0a125e1f68c9b1c8be36682ffe3d5be2d8885a0b
AnaJazVazquez27 <vazquezanajazmin27@gmail.com>
2023-09-07 00:06:52 -0300

    inicio sesion y registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Conversor from "./components/conversor/Conversor";
 import Prestamos from "./components/prestamos/Prestamos";
+import Registro from "./components/inicio/Registro";
+import Inicio from "./components/inicio/Inicio_sesion";
+
 
 function App() {
   return (
     <>
+      <Inicio />
+      <Registro />
       <Conversor />
       <hr className="mt-2" />
       <Prestamos />
