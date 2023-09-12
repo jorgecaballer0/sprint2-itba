a67e5cf94a9d8bdcc9a9d43d8680b3d66cd5cf3f
AnaJazVazquez27 <vazquezanajazmin27@gmail.com>
2023-09-12 11:10:59 -0300

    inicio sesion y registro responsive

--- src/routes/error.jsx
+++ src/routes/error.jsx
@@ -1,18 +1,28 @@
 import { useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Error() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page" className="justify-center">
-      <h1 className="text-orange-800 font-bold text-4xl">Oh No!</h1>
-      <p className="text-2xl text-blue-600 ">
+    <div id="error-page" className="flex flex-col items-center justify-center">
+      <h1 className=" justify-center text-black font-bold text-4xl mb-3">¡Oh No!</h1>
+      <p className="text-2xl text-black mb-3">
         Lo sentimos mucho, algo salió mal :c
       </p>
-      <p className=" text-blue-900">
+      <p className="text-gray-700">
         <i>{error.statusText || error.message}</i>
       </p>
+
+      <Link to={"/inicio"}>
+        <button
+          className="w-72 flex justify-center items-center gap-2 bg-orange-500 hover:bg-orange-800 text-white font-bold mt-4 py-2 px-16 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+        >
+          Ir a inicio
+        </button>
+      </Link>
     </div>
   );
 }
