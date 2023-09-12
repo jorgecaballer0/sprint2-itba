98cb3a7cd34baadebe37cbcfc0c76466a7666620
Jorge Caballero <jorge.caballero.31.10.98@gmail.com>
2023-09-11 23:36:27 -0300

    Prestamos y conversor nuevo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import React from "react";
-import Conversor from "./components/conversor/Conversor";
-import Prestamos from "./components/prestamos/Prestamos";
-import Header from "./components/header/Header";
-import Inicio from "./components/inicio/Inicio";
-import Footer from "./components/footer/Footer";
-// import Sidebar from "./components/sideBar/Sidebar";
+
 
 function App() {
   return (
     <>
-      <Header />
-      {/* <Sidebar/> */}
-      <Inicio />
-      <Conversor />
-      <Prestamos />
-      <Footer />
+
     </>
   );
 }
 
 export default App;
+
